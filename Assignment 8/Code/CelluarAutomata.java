578c3adf88ae6310ae3e849eead70fc6bb2339ec
Kenneth Cox <kenneth.cox@baesystems.com>
2020-10-26 10:27:49 -0600

    +Added Assignment 8

---
+++ Assignment 8/Code/CelluarAutomata.java
@@ -0,0 +1,11 @@
+class CelluarAutomata{
+    public static void main(String[] args){
+        int size = 100;
+        int[] currentStates = new int[size];
+        int[] nextStates = new int[size];
+
+        final int ALIVE = 1;
+        final int DEAD = 0;
+    }
+
+}
