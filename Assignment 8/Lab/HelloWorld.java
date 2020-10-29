e0ed275f4bb89a55e0c3924dfb172efa8a0c93a7
KenCox94 <Integralcalc1050@gmail.com>
2020-10-29 16:30:14 -0600

    +Added Lab

--- Assignment 8/Lab/HelloWorld.java
+++ Assignment 8/Lab/HelloWorld.java
@@ -1,8 +1,33 @@
-public class HelloWorld{
+import java.util.Scanner;
 
+public class HelloWorld{
 	public static void main(String[] args)
 	{
+		float[] floatAr = {1,2,3};
 		System.out.println("Hello World!");
+		System.out.println(addNumbers(1, 2, 3));
+		System.out.println(floatArray(floatAr));
+		promptZ();		
 	}
 
+	public static int addNumbers(int a, int b, int c){
+		return a + b + c;
+	}
+
+	public static float floatArray(float[] arg){
+		float sum = 0;
+		for(float i : arg){
+			sum += i;
+		}
+		return sum;
+	}
+
+	public static void promptZ(){
+		Scanner ch = new Scanner(System.in);
+		char word;
+		do{
+			System.out.println("Please enter a letter: ");
+			word = ch.next().charAt(0);
+		}while(word != 'z');
+	}
 }
